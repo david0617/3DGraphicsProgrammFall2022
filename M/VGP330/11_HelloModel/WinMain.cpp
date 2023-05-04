0223c817d35db119dbbfa4a2fba735c22b2826d0
david0617 <davidyu18800@gmail.com>
2023-05-03 17:19:08 -0700

    full

---
+++ M/VGP330/11_HelloModel/WinMain.cpp
@@ -0,0 +1,26 @@
+#include <M/Inc/M.h>
+#include "GameState.h"
+
+enum class States : size_t
+{
+    GameState = 0,
+    Cube =1,
+    Rectangle=2,
+    Plane=3,
+    Cylinder=4,
+    Sphere=5
+};
+
+int WINAPI  WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
+{
+    M::App& mainApp = M::MainApp();
+    mainApp.AddState<GameState>((size_t)States::GameState);
+    M::AppConfig config;
+    config.appName = L"Hello Model";
+    config.winWidth = 1200;
+    config.winHeigth =720;
+
+    mainApp.Run(config);
+
+    return 0;
+}
